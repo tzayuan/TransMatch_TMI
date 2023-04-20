63717c63fe4e9f76b3932aa8cdbe6fc5ba25b721
SnowBall <729756854@qq.com>
2023-04-20 09:48:25 +0800

    Create Decoder.py

---
+++ Models/Decoder.py
@@ -0,0 +1,62 @@
+import torch.nn as nn
+import torch
+from .token_performer import Token_performer
+from .Transformer import moving_token_inference, contour_token_inference, token_TransformerEncoder
+
+from typing import Optional, Sequence, Tuple, Type, Union
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class DecoderBlock(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            skip_channels=0,
+            use_batchnorm=True,
+    ):
+        super().__init__()
+        self.conv1 = Conv3dReLU(
+            in_channels + skip_channels + skip_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+        )
+        self.conv2 = Conv3dReLU(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+        )
+        self.conv3 = Conv3dReLU(
+            in_channels + skip_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1,
+            use_batchnorm=use_batchnorm,
+        )
+        self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+
+    def forward(self, x, skip=None, skip2=None):
+        x = self.up(x)
+        if skip is not None:
+            x = torch.cat([x, skip], dim=1)
+        if skip2 is not None:
+            x = torch.cat([x, skip2], dim=1)
+            x = self.conv1(x)
+        if skip2 is None:
+            x = self.conv3(x)
+        x = self.conv2(x)
+        return x
+
+class RegistrationHead(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=1):
+        conv3d = nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        conv3d.weight = nn.Parameter(Normal(0, 1e-5).sample(conv3d.weight.shape))
+        conv3d.bias = nn.Parameter(torch.zeros(conv3d.bias.shape))
+        super().__init__(conv3d)
